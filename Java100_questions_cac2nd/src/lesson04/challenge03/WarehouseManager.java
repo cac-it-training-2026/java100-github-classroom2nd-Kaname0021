66779375f54e1bd0a40e379a414e485bd57c875b
kakawaba <kakawaba@C010423173.zipangu.cac.co.jp>
2026-04-29 18:27:26 +0900

    lesson04 完了しました。

--- Java100_questions_cac2nd/src/lesson04/challenge03/WarehouseManager.java
+++ Java100_questions_cac2nd/src/lesson04/challenge03/WarehouseManager.java
@@ -88,19 +88,22 @@ public class WarehouseManager {
 		System.out.println("Z先輩：");
 		System.out.println("いらっしゃいませ、本日はString型のみの販売になります。\n");
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
 		System.out.print("要素数を入れてください＞");
 
-
 		//ここに入力処理を記述する
-
+		String dataNumstr = reader.readLine();
+		int dataNum = Integer.parseInt(dataNumstr);
 
 		//ここに配列宣言を記述する
-
+		String[] strArray = new String[dataNum];
 
 		//ここに値の入力+代入処理を記述する（for文）
-
+		for (int i = 0; i < strArray.length; i++) {
+			System.out.print("\n代入する値を入れてください＞");
+			strArray[i] = reader.readLine();
+		}
 
 		System.out.println("\nYさん：");
 		System.out.println("...出来ました。\n");
@@ -110,9 +113,10 @@ public class WarehouseManager {
 
 		System.out.println("Yさん：");
 
-
 		//ここに配列の値の出力処理を記述する（for文）
-
+		for (int i = 0; i < strArray.length; i++) {
+			System.out.println(strArray[i]);
+		}
 
 		System.out.println("です。\n");
 
